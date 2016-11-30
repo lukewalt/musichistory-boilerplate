1469c59feae2f085663444a11ad622b093f6161f
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-30 15:54:21 -0600

    added new songs at the beginning and end of the starter array

--- songs.js
+++ songs.js
@@ -1,9 +1,14 @@
+
+
 var songs = [];
 
-songs[songs.length] = "a Walk > by Tycho on the album Dive";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = "Buffalo > by Toro y Mio on the album What For?";
+
+// placed a new song at the beginning and end of the starter array
+songs.unshift("Buffalo > by Toro y Mio on the album What For?");
+songs.push("a Walk > by Tycho on the album Dive");
+console.log(songs);

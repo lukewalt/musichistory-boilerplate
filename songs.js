048f8e9c4f1cb15f2eb751ede257f0665dd4249f
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-17 09:58:46 -0600

    refactored logic

--- songs.js
+++ songs.js
@@ -1,35 +1,8 @@
 
+
+
 var songs = [];
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-// placed a new song at the beginning and end of the starter array
-songs.unshift("Buffalo > by Toro y Mio on the album What For?");
-songs.push("A Walk > by Tycho on the album Dive");
-// console.log(songs);
-
-//added missing genres
-for (var i = 0; i < 1; i++) {
-    songs[0] += " - Alt";
-    songs[1] += " - Rock"
-    songs[2] += " - Elctr"
-    songs[3] += " - Rock"
-    songs[4] += " - Rock"
-    songs[5] += " - Rock"
-    songs[6] += " - Elctr"
-}
-
-//removed unwanted characters from each array
-cleanedArray = [];
-for(var i = songs.length -1; i >= 0 ; i--){
-  cleanedArray[i] = songs[i].replace(/@/, "").replace(/[*]/, "").replace(/>/, "-").replace(/!/, "").replace(/[(]/, "").replace("by ", "").replace("on the album", "-");
-}
-console.log(cleanedArray);
-
+var cleanedArray = [];
 
 
 //created empty object to store new values in the form of arrays
@@ -39,16 +12,53 @@ var library = {
     album: [],
     genre: []
 };
-console.log(library);
+console.log("Library  ", library);
 
 
- //capture elements from the DOM in the form of a NODE List
- var songTitle = document.getElementsByClassName('songTitle');
- var artistDisplay = document.getElementsByClassName('artist');
- var albumDisplay = document.getElementsByClassName('album');
- var genreDisplay = document.getElementsByClassName('genre');
+//capture elements from the DOM in the form of a NODE List
+var songTitle = document.getElementsByClassName('songTitle');
+var artistDisplay = document.getElementsByClassName('artist');
+var albumDisplay = document.getElementsByClassName('album');
+var genreDisplay = document.getElementsByClassName('genre');
 
-var display = function() {
+
+function populateArray() {
+
+    songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+    songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+    songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+    songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+    songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+    // placed a new song at the beginning and end of the starter array
+    songs.unshift("Buffalo > by Toro y Mio on the album What For?");
+    songs.push("A Walk > by Tycho on the album Dive");
+    // console.log(songs);
+
+    //added missing genres
+    for (var i = 0; i < 1; i++) {
+        songs[0] += " - Alt";
+        songs[1] += " - Rock"
+        songs[2] += " - Elctr"
+        songs[3] += " - Rock"
+        songs[4] += " - Rock"
+        songs[5] += " - Rock"
+        songs[6] += " - Elctr"
+    }
+    regexArray();
+}
+
+
+//removed unwanted characters from each array
+function regexArray(){
+    for(var i = songs.length -1; i >= 0 ; i--){
+        cleanedArray[i] = songs[i].replace(/@/, "").replace(/[*]/, "").replace(/>/, "-").replace(/!/, "").replace(/[(]/, "").replace("by ", "").replace("on the album", "-");
+    }
+    console.log("Cleaned Array  ", cleanedArray);
+    populateObj()
+}
+
+function populateObj() {
 
     for (var i = 0; i < cleanedArray.length; i++) {
         var songInfoSplit = cleanedArray[i].split("-");
@@ -59,7 +69,10 @@ var display = function() {
         library.genre.push(songInfoSplit[3]);
     }
     console.log(library);
+    display()
+}
 
+function display() {
     //looping throught the arrays stored as object values and setting them equal to the counter indexing each node in the Node list
     for (var j = 0; j < library.songs.length; j++) {
         console.log(songTitle[j]);
@@ -80,7 +93,7 @@ var display = function() {
 
 }
 
-display()
+populateArray()
 
 $('#add-nav').click(function(){
     $('.view-music').addClass('hide');

8a377d80abb6ce216271d693fe456b97a7cdea69
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-18 17:12:26 -0600

    added delete button to each div and added logic to remove clicked div from the DOM

--- songs.js
+++ songs.js
@@ -1,42 +1,19 @@
+//created empty object to store new values in the form of arrays
+var library = {
+    songs: [],
+    artist: [],
+    album: [],
+    genre: []
+};
+console.log("Library  ", library);
 
-var songs = [];
-var cleanedArray = [];
-//
-// function populateArray() {
-//
-//     songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-//     songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-//     songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-//     songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-//     songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-//
-//     // placed a new song at the beginning and end of the starter array
-//     songs.unshift("Buffalo > by Toro y Mio on the album What For?");
-//     songs.unshift("A Walk > by Tycho on the album Dive");
-//     // console.log(songs);
-//
-//     //added missing genres
-//     for (var i = 0; i < 1; i++) {
-//         songs[0] += " - Alt";
-//         songs[1] += " - Rock"
-//         songs[2] += " - Elctr"
-//         songs[3] += " - Rock"
-//         songs[4] += " - Rock"
-//         songs[5] += " - Rock"
-//         songs[6] += " - Elctr"
-//     }
-//     regexArray();
-// }
-//
-//
-// //removed unwanted characters from each array
-// function regexArray(){
-//     for(var i = songs.length -1; i >= 0 ; i--){
-//         cleanedArray[i] = songs[i].replace(/@/, "").replace(/[*]/, "").replace(/>/, "-").replace(/!/, "").replace(/[(]/, "").replace("by ", "").replace("on the album", "-");
-//     }
-//     console.log("Cleaned Array  ", cleanedArray);
-//     populateObj()
-// }
+
+//capture elements from the DOM in the form of a NODE List
+var songTitle = document.getElementsByClassName('songTitle');
+var artistDisplay = document.getElementsByClassName('artist');
+var albumDisplay = document.getElementsByClassName('album');
+var genreDisplay = document.getElementsByClassName('genre');
+var deleteSongArray = document.getElementsByClassName('delete-song')
 
 var globalData;
 
@@ -55,35 +32,16 @@ function getSongs(e) {
 }
 
 
-
-//created empty object to store new values in the form of arrays
-var library = {
-    songs: [],
-    artist: [],
-    album: [],
-    genre: []
-};
-console.log("Library  ", library);
-
-
-//capture elements from the DOM in the form of a NODE List
-var songTitle = document.getElementsByClassName('songTitle');
-var artistDisplay = document.getElementsByClassName('artist');
-var albumDisplay = document.getElementsByClassName('album');
-var genreDisplay = document.getElementsByClassName('genre');
-
-
 function populateObj(globalData) {
-
     for (var i = 0; i < globalData.songs.length; i++) {
-        // taking split items and pushing them to arrays in library object
-        library.songs.push(songInfoSplit[0]);
-        library.artist.push(songInfoSplit[1]);
-        library.album.push(songInfoSplit[2]);
-        library.genre.push(songInfoSplit[3]);
+
+        library.songs.push(globalData.songs[i].song);
+        library.artist.push(globalData.songs[i].artist);
+        library.album.push(globalData.songs[i].album);
+        library.genre.push(globalData.songs[i].genre);
     }
     console.log(library);
-    display()
+    display();
 }
 
 
@@ -92,21 +50,21 @@ var songsDisplayed = document.getElementById("songs")
 function display() {
     //looping throught the arrays stored as object values and setting them equal to the counter indexing each node in the Node list
     var appendedSongs = "";
-    console.log(library.artist);
+    console.log(library.songs);
     for (var i = 0; i < library.songs.length; i++) {
         appendedSongs += `<div class="indvSong">
                             <h2 class="songTitle">${library.songs[i]}</h2>
                             <p class="artist">${library.artist[i]}</p>
                             <p class="album">${library.album[i]}</p>
                             <p class="genre">${library.genre[i]}</p>
+                            <button type="button" name="button" class="delete-song">Delete</button>
                            </div>`
     }
 
     songsDisplayed.innerHTML = appendedSongs
-
+    deleteSongs();
 }
 
-populateArray()
 addMusicButton()
 
 var viewMusicNav = document.getElementById('main-nav');
@@ -156,13 +114,15 @@ function clearInputField() {
 }
 
 
+function deleteSongs() {
+    for (var i = 0; i < deleteSongArray.length; i++) {
+        deleteSongArray[i].addEventListener("click", removal)
+    }
+}
 
-
-
-
-
-
-
+function removal(e){
+    e.target.parentNode.remove();
+}
 
 
 
